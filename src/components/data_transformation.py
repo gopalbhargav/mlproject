6f56993c313a0237c165b3599ddff04f6e289ded
Gopal Bharagv <gopalbhargav59@gmail.com>
2026-03-21 22:27:01 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -12,16 +12,17 @@ from src.logger import logging
 import os
 from src.utils import save_object
 
-
-@dataclass
+## Config Class
+@dataclass  ## auto constructor bnata hai clean code
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join("artifacts", "preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join("artifacts", "preprocessor.pkl") # safe path bnata hai
 
+#main Class
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+        self.data_transformation_config=DataTransformationConfig() 
 
-    def get_data_transformer_object(self):
+    def get_data_transformer_object(self): # preproessing func
 
         '''This function is responsible for data transformation
         '''
@@ -41,7 +42,7 @@ class DataTransformation:
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler(with_mean=False))
+                    ("scaler",StandardScaler(with_mean=False)) ## ohe sparse matrix deta hai and scaler mean substract krta hai 
                 ]
             )
 
@@ -62,21 +63,21 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
 
-    def initiate_data_transformation(self,train_path,test_path):
+    def initiate_data_transformation(self,train_path,test_path): # main transformation func
 
         try:
-            train_df=pd.read_csv(train_path)
+            train_df=pd.read_csv(train_path) ## data load
             test_df=pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
             logging.info("Obtaining preprocessing object")
 
-            Preprocessing_obj=self.get_data_transformer_object()
+            Preprocessing_obj=self.get_data_transformer_object()   # pipeline get
 
             target_column_name="math_score"
             numerical_columns=['reading_score', 'writing_score']
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name])
+            input_feature_train_df=train_df.drop(columns=[target_column_name])  # split features and target
             target_feature_train_df=train_df[target_column_name]
 
             input_feature_test_df=test_df.drop(columns=[target_column_name])
@@ -86,24 +87,23 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=Preprocessing_obj.fit_transform(input_feature_train_df)
-            
+            input_feature_train_arr=Preprocessing_obj.fit_transform(input_feature_train_df) ## apply transformation
             input_feature_test_arr=Preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr=np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            train_arr=np.c_[input_feature_train_arr, np.array(target_feature_train_df)]  ## Combine X & y for train and test
             test_arr=np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object")
 
             save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,   # save preprocessor by func call from utils.py
                 obj=Preprocessing_obj
             )
 
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path   # return jo aage model traing me jayega
             )
         except Exception as e:
             raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -5,15 +5,40 @@ import numpy as np
 import pandas as pd
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_object(file_path,obj):  ## For save the pickle file
     try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
+        dir_path=os.path.dirname(file_path)  ## Full path se folder path nikalna
+        os.makedirs(dir_path, exist_ok=True)  # folder create 
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)  ## dump- save in this specific file path 
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train) # Train model
+
+    # Make predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
     except Exception as e:
         raise CustomException(e,sys)
